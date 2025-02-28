c4aa293de681a4adc7eed8d6e8df9ebf31424eea
d33panpaudel07 <dodeepan7@gmail.com>
2025-02-28 20:50:24 +0545

    basics of binding

--- src/app/components/one-way-binding/one-way-binding.component.ts
+++ src/app/components/one-way-binding/one-way-binding.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ReceivingComponentComponent } from '../receiving-component/receiving-component.component';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-one-way-binding',
   standalone: true,
-  imports: [ReceivingComponentComponent, FormsModule],
+  imports: [FormsModule],
   templateUrl: './one-way-binding.component.html',
   styleUrl: './one-way-binding.component.css',
 })
 export class OneWayBindingComponent {
-  userInput!: string;
+  // avoid using viewChild for DOM manipulation
+  // @ViewChild('testPara') testPara!: ElementRef;
+
+  userInput: string = '';
+  textColor!: string;
+
+  updateText(event: Event) {
+    let inputElem = event.target as HTMLInputElement;
+    this.userInput = inputElem.value;
+    this.textColor = 'blue';
+  }
 }
--- src/app/components/two-way-binding/two-way-binding.component.ts
+++ src/app/components/two-way-binding/two-way-binding.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReceivingComponentComponent } from "../receiving-component/receiving-component.component";
 
 @Component({
   selector: 'app-two-way-binding',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ReceivingComponentComponent],
   templateUrl: './two-way-binding.component.html',
   styleUrl: './two-way-binding.component.css',
 })
@@ -12,6 +13,7 @@ export class TwoWayBindingComponent {
   title: string = 'angularBasics';
   place: string = 'ghar';
   counter: number = 1;
+  userInput!: string;
 
   testFunc = () => {
     console.log('Value of counter is: ' + this.counter++);
